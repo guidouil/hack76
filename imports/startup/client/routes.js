8e521e0634047ac74171d1838de537ad9f143288
Guidouil <guidouil@gmail.com>
2017-10-07 17:45:18 +0200

    Admin

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -3,7 +3,10 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 // Import needed templates
 import '../../ui/layouts/body/body.js';
+import '../../ui/layouts/admin/admin.js';
+
 import '../../ui/pages/home/home.js';
+import '../../ui/pages/admin/admin.js';
 import '../../ui/pages/not-found/not-found.js';
 
 // Set up all routes in the app
@@ -11,6 +14,13 @@ FlowRouter.route('/', {
   name: 'App.home',
   action() {
     BlazeLayout.render('App_body', { main: 'App_home' });
+  },
+});
+
+FlowRouter.route('/admin', {
+  name: 'App.admin',
+  action() {
+    BlazeLayout.render('App_admin', { main: 'admin' });
   },
 });
 
---
+++ imports/ui/layouts/admin/admin.js
@@ -0,0 +1,3 @@
+import './admin.html';
+
+import '../../components/header/header_admin.js';
