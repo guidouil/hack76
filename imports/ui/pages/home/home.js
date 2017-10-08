bbf9cb0032f66ce68fc8d800572464559a9a0ac3
Guidouil <guidouil@gmail.com>
2017-10-08 03:24:38 +0200

    Tablet routes

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -1,4 +1,25 @@
+import { Categories } from '/imports/api/categories/categories.js';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
 import './home.html';
 
 import '../../components/hello/hello.js';
 import '../../components/info/info.js';
+
+
+Template.App_home.onCreated(() => {
+  const instance = this;
+  Meteor.subscribe('categories');
+});
+
+Template.App_home.helpers({
+  categories() {
+    return Categories.find().fetch();
+  },
+});
+
+Template.App_home.events({
+  'click .categoryIcon'() {
+    FlowRouter.go(`/arbo/${this._id}`);
+  }
+});
